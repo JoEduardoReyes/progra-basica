71474b4a81465a401e5f2ab6a2fc44c1491c5f45
Eduardo Reyes <joeduardoreyes@outlook.com>
2025-06-14 11:25:47 -0600

    Creación de un Servidor Básico con Express.js en Node.js

--- mokepon/index.js
+++ mokepon/index.js
@@ -1,1 +1,11 @@
-console.log("Mokepon Game Initialized");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hola, este es mi primer servidor con Node.js");
+});
+
+app.listen(8080, () => {
+	console.log("Server is running on port 8080");
+});
